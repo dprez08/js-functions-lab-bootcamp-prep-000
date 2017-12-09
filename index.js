9829728ca8ed4d62ac87b4bfd1f06233431d0716
David Perez <davperez89@gmail.com>
2017-12-09 05:58:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function happyHolidays(string) {return "Happy holidays!"}
 function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
 function happyHolidayTo(holiday, name){return `Happy ${holiday}, ${name}!`}
 
-function holidayCountdown(days, holiday){return `It's ${days} until ${holiday}!`}
+function holidayCountdown(holiday, days){return `It's ${days} until ${holiday}!`}
 
